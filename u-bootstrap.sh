0c3dc7f243f8f18c9a2972438c19c3a761e21e22
Lolmc0587 <dinhmaiductrong@gmail.com>
2025-07-16 15:28:09 +0700

    Change to mesa 25.0.7

--- u-bootstrap.sh
+++ u-bootstrap.sh
@@ -149,11 +149,11 @@ chroot $1 /bin/bash -c "curl -fsS https://repo.waydro.id | bash && apt install w
 chroot $1 /bin/bash -c "curl -fsS https://dl.brave.com/install.sh | sh"
 # mesa
 mkdir $1/bbb
-# chroot $1 /bin/bash -c "cd bbb && git clone --depth 1 https://gitlab.freedesktop.org/mesa/drm && cd drm/ && mkdir build && cd build/ && meson && ninja install"
-# chroot $1 /bin/bash -c "cd bbb && git clone --depth 1 -b mesa-25.1.5 https://gitlab.freedesktop.org/mesa/mesa.git && cd mesa && mkdir build && cd build && meson -Dvulkan-drivers=panfrost -Dgallium-drivers=panfrost -Dlibunwind=false -Dprefix=/opt/panfrost && ninja install && echo /opt/panfrost/lib/aarch64-linux-gnu | tee /etc/ld.so.conf.d/0-panfrost.conf && echo 'VK_DRIVER_FILES="/opt/panfrost/share/vulkan/icd.d/panfrost_icd.aarch64.json"' >> /etc/environment"
-chroot $1 add-apt-repository ppa:kisak/kisak-mesa
-chroot $1 apt-get update
-chroot $1 apt-get dist-upgrade -y
+chroot $1 /bin/bash -c "cd bbb && git clone --depth 1 https://gitlab.freedesktop.org/mesa/drm && cd drm/ && mkdir build && cd build/ && meson && ninja install"
+chroot $1 /bin/bash -c "cd bbb && git clone --depth 1 -b mesa-25.0.7 https://gitlab.freedesktop.org/mesa/mesa.git && cd mesa && mkdir build && cd build && meson -Dvulkan-drivers=panfrost -Dgallium-drivers=panfrost -Dlibunwind=false -Dprefix=/opt/panfrost && ninja install && echo /opt/panfrost/lib/aarch64-linux-gnu | tee /etc/ld.so.conf.d/0-panfrost.conf && echo 'VK_DRIVER_FILES="/opt/panfrost/share/vulkan/icd.d/panfrost_icd.aarch64.json"' >> /etc/environment"
+# chroot $1 add-apt-repository ppa:kisak/kisak-mesa
+# chroot $1 apt-get update
+# chroot $1 apt-get dist-upgrade -y
 
 echo "DISK usage"
 df $1  
