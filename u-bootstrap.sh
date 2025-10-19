c83b444ecee3fc36449d6b64a916f1cc55bead16
Lolmc0587 <dinhmaiductrong@gmail.com>
2025-10-19 07:16:13 +0700

    Fix mesa

--- u-bootstrap.sh
+++ u-bootstrap.sh
@@ -159,7 +159,7 @@ chroot $1 /bin/bash -c "curl -fsS https://dl.brave.com/install.sh | sh"
 # mesa
 mkdir $1/bbb
 chroot $1 /bin/bash -c "cd bbb && git clone --depth 1 https://gitlab.freedesktop.org/mesa/drm && cd drm/ && mkdir build && cd build/ && meson && ninja install"
-chroot $1 /bin/bash -c "cd bbb && git clone --depth 1 -b mesa-25.2.4 https://gitlab.freedesktop.org/mesa/mesa.git && cd mesa && mkdir build && cd build && meson -Dvulkan-drivers=panfrost -Dgallium-drivers=panfrost,rocket -Dlibunwind=false -Dteflon=true -Dprefix=/opt/panfrost && ninja install && echo /opt/panfrost/lib/aarch64-linux-gnu | tee /etc/ld.so.conf.d/0-panfrost.conf && echo 'VK_DRIVER_FILES="/opt/panfrost/share/vulkan/icd.d/panfrost_icd.aarch64.json"' >> /etc/environment"
+chroot $1 /bin/bash -c "cd bbb && git clone --depth 1 -b mesa-25.2.4 https://gitlab.freedesktop.org/mesa/mesa.git && cd mesa && mkdir build && cd build && meson -Dvulkan-drivers=panfrost -Dgallium-drivers=panfrost -Dlibunwind=false -Dteflon=true -Dprefix=/opt/panfrost && ninja install && echo /opt/panfrost/lib/aarch64-linux-gnu | tee /etc/ld.so.conf.d/0-panfrost.conf && echo 'VK_DRIVER_FILES="/opt/panfrost/share/vulkan/icd.d/panfrost_icd.aarch64.json"' >> /etc/environment"
 
 echo "DISK usage"
 df $1  
